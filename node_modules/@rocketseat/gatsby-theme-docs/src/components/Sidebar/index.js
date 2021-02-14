0da49bd80385d7195f5386d83b51062f6fbc3502
Tanay Nistala <nistalatanay@gmail.com>
2021-02-14 10:42:21 +0530

    Removed Rocketseat module from .gitignore

---
+++ node_modules/@rocketseat/gatsby-theme-docs/src/components/Sidebar/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import { useSidebar } from '@rocketseat/gatsby-theme-docs-core';
+
+import {
+  Container,
+  BioContainer,
+  List,
+  Heading,
+  Item,
+  SubItem,
+} from './styles';
+import { isExternalUrl } from '../../util/url';
+import ExternalLink from './ExternalLink';
+import InternalLink from './InternalLink';
+import BioIcon from './BioIcon'
+
+function ListWithSubItems({ children, text }) {
+  return (
+    <>
+      <Heading>{text}</Heading>
+      <SubItem>{children}</SubItem>
+    </>
+  );
+}
+
+export default function Sidebar({ isMenuOpen }) {
+  const {
+    site: {
+      siteMetadata: { basePath },
+    },
+  } = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          basePath
+        }
+      }
+    }
+  `);
+
+  const data = useSidebar();
+
+  function renderLink(link, label) {
+    return isExternalUrl(link) ? (
+      <ExternalLink link={link} label={label} />
+    ) : (
+      <InternalLink link={link} label={label} />
+    );
+  }
+
+  return (
+    <Container isMenuOpen={isMenuOpen}>
+      <BioContainer>
+        <BioIcon/>
+        <h2>John Doe</h2>
+        <p>Lorem ipsum dolor sit amet</p>
+      </BioContainer>
+      <nav>
+        <List>
+          {data.map(({ node: { label, link, items, id } }) => {
+            if (Array.isArray(items)) {
+              const subitems = items.map((item) => {
+                return (
+                  <Item key={item.link}>
+                    {renderLink(item.link, item.label)}
+                  </Item>
+                );
+              });
+
+              return (
+                <ListWithSubItems key={id} text={label}>
+                  {subitems}
+                </ListWithSubItems>
+              );
+            }
+
+            return <Item key={id}>{renderLink(link, label)}</Item>;
+          })}
+        </List>
+      </nav>
+    </Container>
+  );
+}
+
+ListWithSubItems.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.node,
+  ]).isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+Sidebar.propTypes = {
+  isMenuOpen: PropTypes.bool.isRequired,
+};
