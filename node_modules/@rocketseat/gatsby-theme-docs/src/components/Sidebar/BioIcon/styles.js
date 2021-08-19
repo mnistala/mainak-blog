6551d606e15a0b633b1fca66fb5616e32c026ef0
Mainak Nistala <mnistala@gmail.com>
2021-08-19 07:23:03 +0530

    node-modules added

--- node_modules/@rocketseat/gatsby-theme-docs/src/components/Sidebar/BioIcon/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const CircleImage = styled.img`
-    padding-left: 0;
-    width: 75px;
-    height: 75px;
-    border-radius: 100%;
-    margin-left: 30px;
-`
