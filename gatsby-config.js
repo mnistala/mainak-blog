31d4b742f56ce9079330bd43248e0cb54ecccce2
Mainak Nistala <mnistala@gmail.com>
2021-08-19 20:26:59 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteTitle: `Custom Blog`,
-    defaultTitle: `Custom Blog`,
-    siteTitleShort: `Custom Blog`,
-    siteDescription: `A custom blog based on a documentation template`,
-    siteUrl: `https://rocketdocs.netlify.com`,
+    siteTitle: `Rocket Docs`,
+    defaultTitle: `Rocket Docs`,
+    siteTitleShort: `Rocket Docs`,
+    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
+    siteUrl: `https://rocketdocs.netlify.app`,
     siteAuthor: `@rocketseat`,
     siteImage: `/banner.png`,
     siteLanguage: `en`,
     themeColor: `#8257E6`,
     basePath: `/`,
   },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
     {
       resolve: `@rocketseat/gatsby-theme-docs`,
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
+        repositoryUrl: `https://github.com/rocketseat/gatsby-themes`,
         baseDir: `examples/gatsby-theme-docs`,
       },
     },
@@ -33,17 +34,17 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // trackingId: ``,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
     `gatsby-plugin-remove-trailing-slashes`,
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://rocketdocs.netlify.com`,
+        siteUrl: `https://rocketdocs.netlify.app`,
       },
     },
     `gatsby-plugin-offline`,
