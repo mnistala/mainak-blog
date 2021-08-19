df6cda9aa63ca14ccab3fc3edbd47265a60f0ddf
Mainak Nistala <mnistala@gmail.com>
2021-08-19 20:44:04 +0530

    initial commit

--- src/components/Sidebar/BioIcon/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BioImage from './bioimage.jpg'
-import { CircleImage } from './styles'
-
-export default function BioIcon(props) {
-  return (
-    <CircleImage src={BioImage} alt="Bio Image" />
-  );
-}
--- src/styles/theme.js
+++
@@ -1,9 +0,0 @@
-export default {
-  colors: {
-    primary: '#8257E6',
-    background: '#FFFFFF',
-    shape: `#F2F2FA`,
-    title: `#3D3D4D`,
-    text: `#6C6C80`,
-  },
-};
